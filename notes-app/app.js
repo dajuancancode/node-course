15ea163b07c1665cc110b91589eb0d6f7c63b752
DaJuan Harris <dajuancancode@gmail.com>
2019-06-17 18:11:01 -0400

    Add validator and chalk to package.json

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,3 +1,14 @@
-const notes = require('./notes');
+const validator = require('validator')
+const chalk = require('chalk')
+const notes = require('./notes')
 
-console.log(notes.getNotes());
+const validEmail = validator.isEmail('dajuancancode@gmail.com') ? 'Valid Email' : 'Invalid Email'
+const inValidEmail = validator.isEmail('@gmail.com') ? 'Valid Email' : 'Invalid Email'
+
+const validEmailStyle = chalk.green.inverse.bold
+const inValidEmailStyle = chalk.red.inverse.bold
+
+console.log(notes.getNotes())
+
+console.log(validEmailStyle(validEmail))
+console.log(inValidEmailStyle(inValidEmail))
