564ee8cb1cd1161a28f0006def7d46f63623d773
DaJuan Harris <dajuancancode@gmail.com>
2019-07-04 12:07:47 -0400

    Add routes for task and user creation

---
+++ task-manager/src/models/user.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose')
+const validator = require('validator')
+
+const User = mongoose.model('User', {
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  age: {
+    type: Number,
+    default: 0,
+    validate(value) {
+      if (value < 0) {
+        throw new Error("Age can not be negative")
+      }
+    }
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {throw new Error('Must enter a valid email')}
+    }
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 7,
+    trim: true,
+    validate(value) {
+      if (value.toLowerCase().includes("password")) {
+        throw new Error("Password can not contain 'password'")
+      }
+    }
+  }
+})
+
+module.exports = User
